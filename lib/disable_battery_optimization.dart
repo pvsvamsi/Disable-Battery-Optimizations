877290b30b12a6b38ff8aca455962ef2d95888c7
Paul Imoke <pauleke65@gmail.com>
2022-02-07 11:49:09 +0100

    Added support for null safety

--- lib/disable_battery_optimization.dart
+++ lib/disable_battery_optimization.dart
@@ -3,37 +3,51 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class DisableBatteryOptimization {
-  static const MethodChannel _channel = const MethodChannel('in.jvapps.disable_battery_optimization');
+  static const MethodChannel _channel =
+      const MethodChannel('in.jvapps.disable_battery_optimization');
 
-  static Future<bool> showEnableAutoStartSettings(String dialogTitle, String dialogBody) async {
-    return await _channel.invokeMethod('showEnableAutoStart', <dynamic>[dialogTitle, dialogBody]);
+  static Future<bool?> showEnableAutoStartSettings(
+      String dialogTitle, String dialogBody) async {
+    return await _channel.invokeMethod(
+        'showEnableAutoStart', <dynamic>[dialogTitle, dialogBody]);
   }
 
-  static Future<bool> showDisableManufacturerBatteryOptimizationSettings(String dialogTitle, String dialogBody) async {
-    return await _channel.invokeMethod('showDisableManBatteryOptimization', <dynamic>[dialogTitle, dialogBody]);
+  static Future<bool?> showDisableManufacturerBatteryOptimizationSettings(
+      String dialogTitle, String dialogBody) async {
+    return await _channel.invokeMethod('showDisableManBatteryOptimization',
+        <dynamic>[dialogTitle, dialogBody]);
   }
 
-  static Future<bool> showDisableBatteryOptimizationSettings() async {
+  static Future<bool?> showDisableBatteryOptimizationSettings() async {
     return await _channel.invokeMethod('showDisableBatteryOptimization');
   }
 
-  static Future<bool> showDisableAllOptimizationsSettings(String autoStartTitle, String autoStartBody, String manBatteryTitle, String manBatteryBody) async {
-    return await _channel.invokeMethod('disableAllOptimizations', <dynamic>[autoStartTitle, autoStartBody, manBatteryTitle, manBatteryBody]);
+  static Future<bool?> showDisableAllOptimizationsSettings(
+      String autoStartTitle,
+      String autoStartBody,
+      String manBatteryTitle,
+      String manBatteryBody) async {
+    return await _channel.invokeMethod('disableAllOptimizations', <dynamic>[
+      autoStartTitle,
+      autoStartBody,
+      manBatteryTitle,
+      manBatteryBody
+    ]);
   }
 
-  static Future<bool> get isAutoStartEnabled async {
+  static Future<bool?> get isAutoStartEnabled async {
     return await _channel.invokeMethod("isAutoStartEnabled");
   }
 
-  static Future<bool> get isBatteryOptimizationDisabled async {
+  static Future<bool?> get isBatteryOptimizationDisabled async {
     return await _channel.invokeMethod("isBatteryOptimizationDisabled");
   }
 
-  static Future<bool> get isManufacturerBatteryOptimizationDisabled async {
+  static Future<bool?> get isManufacturerBatteryOptimizationDisabled async {
     return await _channel.invokeMethod("isManBatteryOptimizationDisabled");
   }
 
-  static Future<bool> get isAllBatteryOptimizationDisabled async {
+  static Future<bool?> get isAllBatteryOptimizationDisabled async {
     return await _channel.invokeMethod("isAllOptimizationsDisabled");
   }
 }
